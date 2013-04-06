92f7509066223fba570e902da60295ccd91debc4
hslee16 <alee@Alexs-MacBook-Air.local>
2013-04-06 14:46:38 -0700

    adding sqlite stuff

--- nokoscraper/Street_Info.rb
+++ nokoscraper/Street_Info.rb
@@ -5,6 +5,24 @@ require 'nokogiri'
 require 'open-uri'
 require 'sqlite3'
 
+
+#Create sqlite db and the schema
+
+begin
+  
+  db = SQLite3::Database.open "streets.sqlite3"
+  db.execute "CREATE TABLE IF NOT EXISTS Streets(name text primary key, history text)"
+
+rescue SQLITE3::Exception => e
+
+  puts "Excpetion occurred"
+  puts e
+ 
+ensure 
+  db.close if db
+end
+  
+
 #Get street name and info from www.stgenealogy.com and populate sqlite db
 sfgen_doc = Nokogiri::HTML(open('http://www.sfgenealogy.com/sf/history/hgstr.htm'))
 
